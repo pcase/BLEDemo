777cf8f46a6f6a72ceae6d2532e1d96de388404a
Patty Case <beadypatty@yahoo.com>
2019-03-20 11:19:34 -0700

    Initial Commit

---
+++ BLEDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BLEDemo
+//
+//  Created by Patty Case on 3/20/19.
+//  Copyright © 2019 Azure Horse Creations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
